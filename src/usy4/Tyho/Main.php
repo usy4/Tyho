17e161ac80681b9e613f54ab27358e5d60f021e8
usy4 <68358524+usy4@users.noreply.github.com>
2022-11-27 10:31:15 +0300

    Create Main.php

---
+++ src/usy4/Tyho/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace usy4\Tyho;
+
+use usy4\SwapBall\commands\TyhoCommand;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Skin;
+
+class Main extends PluginBase {
+
+  public function onEnable() : void{
+    foreach(["tyho.json", "tyhoSlim.json"] as $files){
+      $this->saveResource("geo" . "/" . $files);
+    }
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getCommandMap()->register($this->getName(), new TyhoCommand($this));
+  }
+     
+  public function setSkin(Player $player){    
+    $skin = $player->getSkin();      
+    if($player->getSkin()->getGeometryName() == "geometry.humanoid.customSlim"){                                   
+      $player->setSkin(new Skin($skin->getSkinId(), $skin->getSkinData(), "", "geometry.tyhoSlim", file_get_contents($this->getDataFolder() . "geo" . "/" . "tyhoSlim" . ".json")));     
+    } else {
+      $player->setSkin(new Skin($skin->getSkinId(), $skin->getSkinData(), "", "geometry.tyho", file_get_contents($this->getDataFolder() . "geo" . "/" . "tyho" . ".json")));
+    }   
+    $sender->sendSkin();
+  }
+    
+}
